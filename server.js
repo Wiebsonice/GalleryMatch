54770d6f59ed357bd4b06b42f81df7833c1c926d
Wiebe Kremer <wiebe.kremer96@gmail.com>
2019-05-14 13:46:43 +0200

    initial instal of ejs

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/about', (req, res) => res.send('This is about page'))
+app.set('view engine', 'ejs');
+app.set('views', 'view')
 
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/about', function(req, res) {
+    res.render('about');
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
