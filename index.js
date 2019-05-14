3ada1105fda5a10d574ecdb23495d1ed684743fe
Wiebe Kremer <wiebe.kremer96@gmail.com>
2019-05-14 13:19:19 +0200

    Updating folder index

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/abbout', (req, res) => res.send('This is about page'))
+app.get('/about', (req, res) => res.send('This is about page'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
