17e4062e5a6dbf9b027fb4d94df3465459d5006b
Wiebe Kremer <wiebe.kremer96@gmail.com>
2019-05-16 10:30:20 +0200

    404 route added with ejs template

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// allow cross orgin refrence
+// allow cross orgin resource serving
 app.use(cors({origin: '*'}));
 
 app.set('view engine', 'ejs');
@@ -16,4 +16,10 @@ app.get('/', function(req, res) {
 app.get('/account', function(req, res) {
     res.render('account', {title: 'Account' });
 });
+
+// 404 status route set.
+app.use(function(req, res, next){
+    res.status(404).render('404', {title: "Sorry, page not found"});
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
